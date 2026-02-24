a48d4bbbc5ae055d4a7ee533f80afabdd33223e3
Laura Ferrando Ferrero <say@heylaura.dev>
2026-02-24 17:43:28 +0100

    chore: install and configure Tailwind

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Plant Your Day 🪴</h1>
-  <p>
+  <h1 class="text-2xl font-bold mb-4 lg:text-4xl">Plant Your Day 🪴</h1>
+  <p class="text-lg lg:text-2xl">
     This is a simple app to help you keep track of your plants and their care (the real kind, not
     the ones you buy from the store 🌿)
   </p>
 </template>
-
-<style scoped>
-h1 {
-  font-size: 2rem;
-  font-weight: 600;
-  margin-bottom: 1rem;
-}
-
-p {
-  font-size: 1.2rem;
-  line-height: 1.5;
-}
-
-@media (min-width: 1024px) {
-  h1 {
-    font-size: 2.5rem;
-  }
-
-  p {
-    font-size: 1.5rem;
-  }
-}
-</style>
